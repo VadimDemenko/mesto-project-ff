1fe1dc59aa5fa2362afb62215f628451561d3ce8
Vadim Demenko <vad-08071999@yandex.ru>
2024-04-23 21:45:19 +0300

    1.0

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsList = document.querySelector('.places__list');
+
+function creatCard(title, image) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardButtonDelete = cardElement.querySelector('.card__delete-button');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = image;
+  cardImage.alt = `На изображении изображено: ${title}`;
+  cardButtonDelete.addEventListener('click', () => {
+    deleteCard(cardElement);
+  })
+  cardTitle.textContent = title;
+
+  return cardElement;
+}
+
+initialCards.forEach( (elem) => {
+  cardsList.append(creatCard(elem.name, elem.link))
+});
+
+function deleteCard(elem) {
+  elem.remove();
+}
